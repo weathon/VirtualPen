a067e9187b8a6d5a7d7fd044af97d0019fc63b38
Wayne <wguo6358@gmail.com>
2020-10-30 12:38:40 -0500

    -

--- demo.cpp
+++ demo.cpp
@@ -9,20 +9,34 @@ VideoCapture cam(0);
 
 int main(int argc, char **argv)
 {
+    // ios_base::sync_with_stdio(true);
     // Mat img = imread("test.jpg");//worked!
     // https://thefreecoder.wordpress.com/2012/09/11/opencv-c-video-capture/
     Mat pic;
     Mat gray;
+    cam.read(pic);
+    Size s = pic.size();
+    int rows = s.height;
+    int cols = s.width;
     while (1)
     {
         cam.read(pic);
         cvtColor(pic, gray, cv::COLOR_RGB2GRAY);
+        for(int x=0;x<rows;x++)
+        {
+            for(int y=0;y<cols;y++)
+            {
+                // Vec &color = gray.at<Vec>(x, y);
+                // color[0]+=100;
+                // gray.at<Vec3b>(x, y)=color;
+                int value = gray.at<uchar>(x,y);
+            }
+        }
         imshow("Image", gray);
-        // Vec3b &color = gray.at<Vec3b>(5, 5);
-        cout << 1;
-        if (waitKey(30) >= 0) //wei sm bu yao bu xing zhi you wait key bu xing
+        // cout << color << endl;
+        // printf("1");
+        if (waitKey(30) == 'q')
             break;
-
         // https://thefreecoder.wordpress.com/2012/09/11/opencv-c-video-capture/
     }
 }
