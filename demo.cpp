da432901a371e8b61d62ae99aec7e6ee901f7094
Wayne <wguo6358@gmail.com>
2020-10-30 12:03:45 -0500

    -

--- demo.cpp
+++ demo.cpp
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
 
 using namespace cv;
+using namespace std;
 VideoCapture cam(0);
 
 int main(int argc, char **argv)
@@ -16,6 +18,8 @@ int main(int argc, char **argv)
         cam.read(pic);
         cvtColor(pic, gray, cv::COLOR_RGB2GRAY);
         imshow("Image", gray);
+        // Vec3b &color = gray.at<Vec3b>(5, 5);
+        cout << 1;
         if (waitKey(30) >= 0) //wei sm bu yao bu xing zhi you wait key bu xing
             break;
 
